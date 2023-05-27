f5f4ca56a0553bc4190e2663ee90d8e83080576f
Simon Reinisch <simon@reinisch.io>
2023-05-27 14:53:19 +0200

    refactor(tests): simplify assertions

--- db_test.go
+++ db_test.go
@@ -11,10 +11,10 @@ import (
 
 var dbIndex = 0
 
-// TODO: simplify
-func testDb(t *testing.T, run func(db *DB)) {
+func testDb(t *testing.T, run func(db *DB, assert *assert.Assertions)) {
+	assert := assert.New(t)
 	dir, err := os.Getwd()
-	assert.Nil(t, err)
+	assert.Nil(err)
 
 	testDir := filepath.Join(dir, "_test")
 	testId := strconv.Itoa(time.Now().Nanosecond()) + "_" + strconv.Itoa(dbIndex)
@@ -23,25 +23,25 @@ func testDb(t *testing.T, run func(db *DB)) {
 
 	err = os.MkdirAll(dbDir, 0777)
 	db, err := Open(dbDir)
-	assert.Nil(t, err)
+	assert.Nil(err)
 
-	assert.Nil(t, err)
+	assert.Nil(err)
 
-	run(db)
+	run(db, assert)
 	err = os.RemoveAll(testDir)
-	assert.Nil(t, err)
+	assert.Nil(err)
 }
 
 func TestOpen(t *testing.T) {
-	testDb(t, func(db *DB) {})
+	testDb(t, func(db *DB, assert *assert.Assertions) {})
 }
 
 func TestDB_Set(t *testing.T) {
-	testDb(t, func(db *DB) {
+	testDb(t, func(db *DB, assert *assert.Assertions) {
 
 		// Set a value
 		err := db.Set([]byte("hello"), []byte("world"))
-		assert.Nil(t, err)
+		assert.Nil(err)
 	})
 }
 
@@ -53,7 +53,7 @@ func TestDB_Get(t *testing.T) {
 		"baz":   "bam bar",
 	}
 
-	testDb(t, func(db *DB) {
+	testDb(t, func(db *DB, assert *assert.Assertions) {
 
 		// Set and get value
 		for key, value := range values {
@@ -61,95 +61,94 @@ func TestDB_Get(t *testing.T) {
 			bValue := []byte(value)
 
 			err := db.Set(bKey, bValue)
-			assert.Nil(t, err)
+			assert.Nil(err)
 
 			result, err := db.Get(bKey)
-			assert.Nil(t, err)
-			assert.Equal(t, bValue, result)
+			assert.Nil(err)
+			assert.Equal(bValue, result)
 		}
 
 		// Read values
 		for key, value := range values {
 			result, err := db.Get([]byte(key))
-			assert.Nil(t, err)
-			assert.Equal(t, result, []byte(value))
+			assert.Nil(err)
+			assert.Equal(result, []byte(value))
 		}
 
 		// Read non-existing values
 		result, err := db.Get([]byte("xyz"))
-		assert.Nil(t, err)
-		assert.Nil(t, result)
-
+		assert.Nil(err)
+		assert.Nil(result)
 	})
 }
 
 func TestDB_Delete(t *testing.T) {
-	testDb(t, func(db *DB) {
+	testDb(t, func(db *DB, assert *assert.Assertions) {
 		db.Set([]byte("foo"), []byte("bar"))
 		db.Set([]byte("baz"), []byte("bam"))
 
 		value, _ := db.Get([]byte("foo"))
-		assert.Equal(t, value, []byte("bar"))
+		assert.Equal(value, []byte("bar"))
 
 		value, _ = db.Get([]byte("baz"))
-		assert.Equal(t, value, []byte("bam"))
+		assert.Equal(value, []byte("bam"))
 
 		status, err := db.Delete([]byte("fo2"))
-		assert.Nil(t, err)
-		assert.False(t, status)
+		assert.Nil(err)
+		assert.False(status)
 
 		status, err = db.Delete([]byte("foo"))
-		assert.Nil(t, err)
-		assert.True(t, status)
+		assert.Nil(err)
+		assert.True(status)
 
 		value, err = db.Get([]byte("foo"))
-		assert.Nil(t, err)
-		assert.Nil(t, value)
+		assert.Nil(err)
+		assert.Nil(value)
 	})
 }
 
 func TestDB_Size(t *testing.T) {
-	testDb(t, func(db *DB) {
+	testDb(t, func(db *DB, assert *assert.Assertions) {
 		db.Set([]byte("foo"), []byte("bar"))
 		db.Set([]byte("baz"), []byte("world"))
 		size, err := db.Stat()
 
-		assert.Nil(t, err)
-		assert.Equal(t, size.dict, uint32(ValuePointerSize*2))
-		assert.Equal(t, size.data, uint32(8))
-		assert.Equal(t, size.entries, uint32(2))
+		assert.Nil(err)
+		assert.Equal(size.dict, uint32(ValuePointerSize*2))
+		assert.Equal(size.data, uint32(8))
+		assert.Equal(size.entries, uint32(2))
 	})
 }
 
 func TestDB_RunGC(t *testing.T) {
-	testDb(t, func(db *DB) {
+	testDb(t, func(db *DB, assert *assert.Assertions) {
 		db.Set([]byte("foo"), []byte("bam"))
 		db.Set([]byte("baz"), []byte("bare"))
 		db.Set([]byte("boo"), []byte("var"))
 
 		size, _ := db.Stat()
-		assert.Equal(t, size.dict, uint32(ValuePointerSize*3))
-		assert.Equal(t, size.data, uint32(10))
-		assert.Equal(t, size.entries, uint32(3))
+		assert.Equal(size.dict, uint32(ValuePointerSize*3))
+		assert.Equal(size.data, uint32(10))
+		assert.Equal(size.entries, uint32(3))
 
 		db.Delete([]byte("foo"))
 		db.Delete([]byte("boo"))
 
 		size, _ = db.Stat()
-		assert.Equal(t, size.dict, uint32(ValuePointerSize*3))
-		assert.Equal(t, size.data, uint32(10))
-		assert.Equal(t, size.entries, uint32(3))
+		assert.Equal(size.dict, uint32(ValuePointerSize*3))
+		assert.Equal(size.data, uint32(10))
+		assert.Equal(size.entries, uint32(3))
 
 		err := db.RunGC()
-		assert.Nil(t, err)
+		assert.Nil(err)
 
 		data, err := db.Get([]byte("baz"))
-		assert.Nil(t, err)
-		assert.Equal(t, data, []byte("bare"))
+		assert.Nil(err)
+		assert.Equal(data, []byte("bare"))
 
 		size, _ = db.Stat()
-		assert.Equal(t, size.dict, uint32(ValuePointerSize))
-		assert.Equal(t, size.data, uint32(4))
-		assert.Equal(t, size.entries, uint32(1))
+		assert.Equal(size.dict, uint32(ValuePointerSize))
+		assert.Equal(size.data, uint32(4))
+		assert.Equal(size.entries, uint32(1))
 	})
 }
